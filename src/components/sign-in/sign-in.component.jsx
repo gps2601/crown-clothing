eb8a23bee28d0c14620800606728dee78fe25dfd
Geoff <gsmit592@ford.com>
2020-03-24 15:49:54 +0000

    sign in now handles email/password sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import FormInput from '../../components/form-input/form-input.component'
 import CustomButton from '../../components/custom-button/custom-button.component'
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { signInWithGoogle, auth } from '../../firebase/firebase.utils';
 
 import "./sign-in.styles.scss";
 
@@ -17,10 +17,16 @@ class SignIn extends Component {
     };
   }
 
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault();
+    const { email, password } = this.state;
 
-    this.setState({ email: "", password: "" });
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch(error){
+      console.log(error)
+    }
   };
 
   handleChange = e => {
