bdb7cc16c3358af427ce3e1e2ae58fa7ed0d2622
Geoff <gsmit592@ford.com>
2020-04-14 22:51:11 +0100

    migrating sign out functionality to use redux saga

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
-
-import { auth } from '../../firebase/firebase.utils';
-
 import { ReactComponent as Logo } from '../../assets/original.svg'
 import CartIcon from '../cart-icon/cart-icon.component'
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import { selectCartHidden } from '../../redux/cart/cart.selectors'
 import { selectCurrentUser } from '../../redux/user/user.selectors'
+import { signOutStart } from '../../redux/user/user.actions'
 
 import { HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink } from './header.styles'
 
-const Header = ( {currentUser, hidden} ) => (
+const Header = ( {currentUser, hidden, signOutStart } ) => (
     <HeaderContainer>
         <LogoContainer className='logo-container' to='/'>
             <Logo className='logo'/>
@@ -26,7 +24,7 @@ const Header = ( {currentUser, hidden} ) => (
         </OptionLink>
         {
             currentUser ?
-            <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
+            <OptionDiv onClick={signOutStart}>SIGN OUT</OptionDiv>
             :
             <OptionLink to="/signin">SIGN IN</OptionLink>
         }
@@ -44,4 +42,8 @@ const mapStateToProps  = createStructuredSelector({
     hidden: selectCartHidden
 })
 
-export default connect(mapStateToProps, null)(Header)
+const mapDispatchToProps = dispatch => ({
+    signOutStart: () => dispatch(signOutStart())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
